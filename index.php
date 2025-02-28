ba5b78ff847810aa9086f3c8067dbd1841d6637d
CS Vision <csvision.edubth@gmail.com>
2025-02-28 20:29:07 +0530

    Update index.php

--- index.php
+++ index.php
@@ -43,7 +43,7 @@
 
     <!-- Navbar Start -->
     <nav class="navbar navbar-expand-lg bg-white navbar-light shadow sticky-top p-0">
-        <a href="index.html" class="navbar-brand d-flex align-items-center px-4 px-lg-5">
+        <a href="index.php" class="navbar-brand d-flex align-items-center px-4 px-lg-5">
             <h2 class="m-0 text-primary">
   <img src="img/Logo.jpg" alt="CS Vision Logo" style="height: 40px; width: auto; margin-right: 10px;">
   CS Vision
